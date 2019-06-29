494745c06b834fe68203d14f494ecc2781ca64bb
Ruslan <sabirov@worksolutions.ru>
2019-06-29 12:22:21 +0300

    Утилита tree

---
+++ tree/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func dirTree(out io.Writer, path string, printFiles bool) error {
+	return nil
+}
